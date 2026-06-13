47393607dab6f1939348c7464254f10ef212069a
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-13 23:03:33 +0300

    setup configurations

--- backend/learning-platform-core/app.py
+++ backend/learning-platform-core/app.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+## configurations ##
+
+app.config.from_object("config")
+
+
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def sayHello():
---
+++ backend/learning-platform-core/config.py
@@ -0,0 +1,3 @@
+DEBUG=True
+SQLALCHEMY_DATABASE_URI='sqlite:///online_learning_platform.db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
