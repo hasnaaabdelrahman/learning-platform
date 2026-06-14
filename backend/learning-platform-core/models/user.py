94cb31e30063ec7fac6b090c444757553441d717
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-14 12:37:14 +0300

    add email column to user table

--- backend/learning-platform-core/models/user.py
+++ backend/learning-platform-core/models/user.py
@@ -8,15 +8,17 @@ class User(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     first_name = db.Column(db.String(10), nullable=False)
     last_name = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(100) , nullable = False , unique=True)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(10), nullable=False)
     enrollments = db.relationship("Enrollment" , backref = "user")
     comments = db.relationship("Comment" , backref = "user")
 
 
-    def __init__(self , first_name , last_name , password , role):
+    def __init__(self , first_name , last_name , password , role , email):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
         self.password = hash_password(password)
         self.role = role
 
