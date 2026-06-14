44eda633c837891ba025b2d407d9be5d8a3b5ebc
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-06-14 16:03:39 +0300

    add swagger docs

--- backend/learning-platform-core/app.py
+++ backend/learning-platform-core/app.py
@@ -1,26 +1,21 @@
 import os
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from extensions import db, migrate
-import models
+from flask_restx import Api , Resource
+from config import DevConfig
 
 
 app = Flask(__name__)
+app.config.from_object(DevConfig)
 
-## configurations ##
-
-app.config.from_object("config")
-
-db.init_app(app)
-migrate.init_app(app , db)
+api= Api(app,doc='/docs')
 
 
 
-@app.route("/")
-def sayHello():
-    return "Hello"
+@api.route("/hello")
+class HelloResource(Resource):
+    def get(self):
+        return {"message":"hello"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- backend/learning-platform-core/config.py
+++ backend/learning-platform-core/config.py
@@ -1,3 +1,24 @@
-DEBUG=True
-SQLALCHEMY_DATABASE_URI='sqlite:///online_learning_platform.db'
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+from decouple import config
+import os
+
+BASE_DIR= os.path.dirname(os.path.realpath(__file__))
+
+class Config:
+    SECRET_KEY= config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS= config('SQLALCHEMY_TRACK_MODIFICATIONS' , cast=bool)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///'+ os.path.join(BASE_DIR , 'online_learning_platform.db')
+    DEBUG=True
+    SQLALCHEMY_ECHO=True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+
+
+
+
+
